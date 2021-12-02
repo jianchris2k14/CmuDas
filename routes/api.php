e114ef5654ac2c3893384fd282612026d0ae16cb
vagrant <vagrant@homestead>
2021-12-02 07:51:20 +0000

    Re:dded sanctum authentication

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\FileController;
 
 // Public Routes
 // Route::resource('files', FileController::class);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+
 Route::get('/files', [FileController::class, 'index']);
 Route::post('/files/{id}', [FileController::class, 'show']);
 Route::get('/files/search/{name}', [FileController::class, 'search']);
@@ -15,6 +19,8 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('/files', [FileController::class, 'store']);
     Route::put('/files/{id}', [FileController::class, 'update']);
     Route::delete('/files/{id}', [FileController::class, 'destroy']);
+
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
