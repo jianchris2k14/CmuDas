6ac0171d69134c4b9f8dbf6dead24bf37b236c09
jianchris2k14 <jianchris2k14@gmail.com>
2021-12-16 06:08:51 +0000

    Landing Page Start

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    return view('homepage/landing');
+});
+
+Route::get('/login', function () {
+    return view('system/login');
+});
+
+Route::get('/dashboard', function () {
+    return view('system/dashboard');
 });
